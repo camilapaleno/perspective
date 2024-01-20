2fb314ecf4fff0661eea3ba1f7e83b2dd50e4003
camilapaleno <88165955+camilapaleno@users.noreply.github.com>
2024-01-19 17:48:40 -0800

    Added navbar, update favicon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import Spline from '@splinetool/react-spline';
 export default function App() {
   return (
     <div className='wrapper'>
-      <Spline className='spline' scene="https://prod.spline.design/i83NqDAy7E6ffKga/scene.splinecode" />
+      <div className='border'>
+        <Spline className='spline' scene="https://prod.spline.design/i83NqDAy7E6ffKga/scene.splinecode" />
+
+        <nav>
+
+          <ul class="nav-links">
+              <li>Home</li>
+              <li>About</li>
+              <li>Portfolio</li>
+              <li>Contact</li>
+          </ul>
+
+        </nav>
+      </div>
+
     </div>
     
   );
